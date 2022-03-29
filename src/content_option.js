45af1d133c129481936a21ed0fd65438bfa4eeaa
Niaz-Ul-Haque <theniaz619@gmail.com>
2022-03-29 13:09:34 -0400

    pic works now but not compelte

--- src/content_option.js
+++ src/content_option.js
@@ -12,30 +12,30 @@ const introdata = {
         third:  "passionate about web dev",
     },
     description: "toronto based web/full-stack dev. clean code enthusiast. \nrecently graduated (summer 2022). not the best gamer tho. ",
-    your_img_url: "",
+    img_url: "https://i.postimg.cc/Twm00rfW/pngkey-com-dr-strange-png-5591712.png",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "he/him. 22 years old (june 27). can speak 6 <br />languages.\n honour/dean's list 2 times. quick learner. \n problem solver. hackathon enjoyer. <br />congenial & committed. scared of dogs & heights. ice breaker. kpop, anime & kdramas. skors mcflurry",
+    aboutme: "he/him. 22 years old (june 27). can speak 6 languages. honour/dean's list 2 times. quick learner. problem solver. hackathon enjoyer. congenial & committed. scared of dogs & heights. ice breaker. kpop, kdramas, & anime.",
 };
 const worktimeline = [{
         jobtitle: "Jr. Web Developer",
         where: "Treasury Board Secretariat - Toronto",
         date: "2021-2022",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id l<br />igula semper elementum feugiat pretium nulla. \nNunc non commodo dolor. \nNunc mollis dignissim facilisis.\n Morbi ut magna ultricies.Nunc mollis dignissim facilisis. Morbi ut magna ultricies.Nunc mollis dignissim facilisis. Morbi ut magna ultricies.Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+        desc:" This needs to be edited. the final one will be perferct, now just for testing (messed up allignemts).\n  Praesent quis dolor id  igula semper elementum feugiat pretium nulla. Nunc non commodo dolor. unc mollis dignissim facilisis.\n Morbi ut magna ultricies.Nunc mollis dignissim facilisis. Morbi ut magna ultricies.Nunc mollis dignissim facilisis. Morbi ut magna ultricies.Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
     },
     {
         jobtitle: "Full Stack Developer",
         where: "Vroom - Toronto",
         date: "2020-2021",
-        desc:"· Designed, coded, and supported software solutions that can easily \nbe scaled depending on client needs\n· Maximized application's efficiency, data quality, scope, operability, and flexibility\n· Participated in user requirements meetings in order to remodel client needs into proposed app designs",
+        desc:"· Designed, coded, and supported software solutions that can easily be scaled depending on client needs\n· Maximized application's efficiency, data quality, scope, operability, and flexibility\n· Participated in user requirements meetings in order to remodel client needs into proposed app designs",
     },
     {
         jobtitle: "Private Tutor",
         where: "Riyadh",
         date: "2015-2018",
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent\n quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi\n ut magna ultricies.Nunc mollis dignissim facilisis. Morbi ut magna ultricies.Nunc mollis dignissim facilisis. Morbi ut magna ultricies.Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+        desc:"This needs to be edited. the final one will be perferct, now just for testing.\n quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi\n ut magna ultricies.Nunc mollis dignissim facilisis. Morbi ut magna ultricies.Nunc mollis dignissim facilisis. Morbi ut magna ultricies.Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
     },
 ];
 
@@ -49,15 +49,15 @@ const skills = [{
     },
     {
         name: "angular",
-        value: 75,
+        value: 80,
+    },
+    {
+        name: "vue",
+        value: 85,
     },
     {
         name: "jquery",
         value: 85,
-    },
-    {
-        name: "vue",
-        value: 80,
     },
     {
         name: "java",
@@ -83,18 +83,18 @@ const skills = [{
 
 const services = [{
         title: "Asahi’sDen",
-        description: "Programmed eCommerce web application following MVC design pattern leveraging Handlebars template engine and Express framework  · Built a responsive design using Bootstrap4 and connected to a database with MongoDB",
+        description: "Again, needs to be eidted. will add github links at the end of each.\n · Programmed eCommerce web application following MVC design pattern leveraging Handlebars template engine and Express framework\n· Built a responsive design using Bootstrap4 and connected to a database with MongoDB",
     },
     {
         title: "Vroom-Rent",
-        description: "Currently developing a WebApp following Agile SDLC for a client that helps user search and rent apartments available with ease· User are able to securely Signup/Login and book an appointment with the landlord/s       · Implemented Google Maps API to the app, to pin point the exact location of the apartment",
+        description: "· Currently developing a WebApp following Agile SDLC for a client that helps user search and rent apartments available with ease\n · User are able to securely Signup/Login and book an appointment with the landlord/s\n· Implemented Google Maps API to the app, to pin point the exact location of the apartment",
     },
     {
         title: "nlinks",
-        description: "Designed and coded an Open-Sourced Node CLI that find’s the status of websites   · Made use of different types of packages from NPM and API’s to automate the CLI        · The CLI can load data from any type of files and overwrite if wanted",
+        description: "· Designed and coded an Open-Sourced Node CLI that find’s the status of websites\n· Made use of different types of packages from NPM and API’s to automate the CLI\n· The CLI can load data from any type of files and overwrite if wanted",
     },{
         title: "COVID-Tracker",
-        description: "· Created a COVID-19 Tracker App using React, ChartsJS, and MaterialUI for the UI Design      · Data is fetched from the COVID-19 API using Async/Await syntax and React Hooks, and        statistics are displayed in cards as well as in charts",
+        description: "· Created a COVID-19 Tracker App using React, ChartsJS, and MaterialUI for the UI Design\n· Data is fetched from the COVID-19 API using Async/Await syntax and React Hooks, and statistics are displayed in cards as well as in charts",
     },
 ];
 
